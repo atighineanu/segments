c56fc80e5e9498c4515d8bbd0c41503ea6b7d566
atighineanu <atighineanu@suse.de>
2021-07-09 10:43:00 +0200

    added marshal with indent for export

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"megaproj1/utils"
 )
 
 func main() {
-
-	intervals := []utils.Interval{{25, 30}, {2, 19}, {14, 23}, {4, 8}}
-	mergedInterval, _ := utils.MERGEI(intervals)
-	fmt.Printf("The result: %v\n", mergedInterval)
+	intrv, _ := utils.MERGEI([]utils.Interval{{-20, 0}, {300, 455}, {1, 2}, {5, 10}, {4, 23}, {4, 54}, {55, 100}, {102, 200}})
+	a, err := json.MarshalIndent(intrv, "", " ")
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+	fmt.Println(string(a))
 }
