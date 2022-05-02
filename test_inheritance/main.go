83bd453c0cd85791a7c78fce4862bd51185b6f78
atighineanu <alexei.tighineanu@gmail.com>
2022-05-02 20:36:32 +0200

    added dodge and take damage functions

--- test_inheritance/main.go
+++ test_inheritance/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Character struct {
 	HP     int
 	Lives  int
 	Armour int
+}
+
+func (c *Character) TakeDmg(damage int) {
+	if damage > c.Armour {
+		c.HP -= damage - c.Armour
+	}
 }
 
 func (C *Character) PutShieldOn(value int) {
@@ -16,13 +26,25 @@ type Enemy struct {
 	Character
 }
 
-func (e *Enemy) Dodge() {
-
+func (e *Enemy) Dodge(rate int) bool {
+	if rate > 100 {
+		fmt.Println("Dodge rate cannot exceed 100%")
+	} else {
+		rand.Seed(time.Now().UnixNano())
+		//fmt.Printf("%v\n", a)
+		if rand.Intn(100) <= rate {
+			fmt.Println("dodged")
+			return true
+		}
+	}
+	fmt.Println("didn't dodge")
+	return false
 }
 
 type Creature struct {
 	Enemy
-	Flies bool
+	Flies     bool
+	DodgeRate int
 }
 
 func (c *Creature) StartFlying() {
@@ -31,8 +53,14 @@ func (c *Creature) StartFlying() {
 
 func (c *Creature) StartFlyingWhileDodgingandCarryingShield() {
 	c.Flies = true
-	c.Dodge()
 	c.PutShieldOn(10)
+	c.DodgeRate += 10
+}
+
+func (c *Creature) TakeDamage(damage int) {
+	if !c.Dodge(c.DodgeRate) {
+		c.TakeDmg(damage)
+	}
 }
 
 func main() {
@@ -47,7 +75,8 @@ func main() {
 	fmt.Printf("%+v\n", troll)
 	troll.PutShieldOn(3)
 	fmt.Printf("%+v\n", troll)
-
+	troll.TakeDmg(7)
+	fmt.Printf("%+v\n", troll)
 	gargoyle := Creature{
 		Enemy{
 			Character{
@@ -57,10 +86,13 @@ func main() {
 			},
 		},
 		false,
+		10,
 	}
 	fmt.Printf("%+v\n", gargoyle)
 	gargoyle.StartFlying()
 	fmt.Printf("%+v\n", gargoyle)
 	gargoyle.StartFlyingWhileDodgingandCarryingShield()
+	fmt.Printf("%+v\n", gargoyle)
+	gargoyle.TakeDamage(20)
 	fmt.Printf("%+v\n", gargoyle)
 }
